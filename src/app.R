d944403b454fc3427d21b78afaaf519e13fd3623
Guanchen Zhang <guanchen@Guanchens-MacBook-Pro.local>
2018-01-20 22:47:16 -0800

    add markers on map

--- src/app.R
+++ src/app.R
@@ -1,8 +1,16 @@
-library(shiny)
+# library(shiny)
 
 server <- (function(input, output, session) {
   # server codes
   # useShinyjs(html = TRUE)
+  
+  # data <- read_csv('../data/scorecard.csv')
+  scorecard <- readRDS('../data/data.Rds')
+  
+  # coords <- tibble(lat = c(47.792927, 40.252928),
+  #                  lon = c(-118.785114, -121.493451))
+  coords <- scorecard[,c('INSTNM', 'lat', 'lon')]
+  session$sendCustomMessage("getCities", coords)
   
   observeEvent(input$selectedState, {
     # message1 = "hello"
